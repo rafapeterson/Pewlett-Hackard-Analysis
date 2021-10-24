ccc3e96ec35530876a1cecb35db1fd32d67ac081
rafapeterson <rafaaapeterson@gmail.com>
2021-10-24 08:36:08 -0700

    added retirement_info.csv

--- schema.sql
+++ schema.sql
@@ -35,14 +35,6 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (title)
-);
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
@@ -52,5 +44,15 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (dept_no, emp_no)
+);
+
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
 );
 
