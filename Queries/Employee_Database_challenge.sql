8395061548480006f84a7c21fc02204273d9a230
rafapeterson <rafaaapeterson@gmail.com>
2021-10-27 08:58:27 -0700

    Add files via upload
    adding resaved version

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -28,3 +28,22 @@ INTO retiring_titles
 FROM filtered_retirement_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+
+-- Mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.first_name,
+    e.last_name,
+    e.birth_date,
+    d.from_date,
+    d.to_date,
+    t.title
+
+FROM employees AS e
+LEFT JOIN dept_emp AS d
+ON e.emp_no = d.emp_no
+LEFT JOIN titles as t
+ON e.emp_no = t.emp_no
+
+--INTO mentorship_eligibilty
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
